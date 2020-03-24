e79cdc9801397534be833b82e7dc78188184d276
Dominic Luidold <dominic.luidold@students.fhv.at>
2020-03-24 12:23:06 +0100

    [Exercise-5] Initial commit for "Image Loader"

---
+++ Exercise-5_ImageLoader/js/popupHandler.js
@@ -0,0 +1,17 @@
+function imagePopup(imagePath) {
+    /* Create new image */
+    let img = new Image();
+    img.src = imagePath;
+
+    img.onload = function () {
+        /* Open popup once image is loaded */
+        let popup = window.open(imagePath, imagePath, `width=${img.width}, height=${img.height}`);
+
+        /* Image closes popup when clicked */
+        popup.onload = function () {
+            popup.document.getElementsByTagName("img")[0].onclick = function () {
+                popup.close();
+            };
+        }
+    };
+}
